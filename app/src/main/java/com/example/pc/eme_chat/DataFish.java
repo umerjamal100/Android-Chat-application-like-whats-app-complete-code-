51f9c3fbde3f986c359e7ad2db5be7b8de5c54d5
umerjamal100 <umerjamal109@gmail.com>
2017-11-19 15:04:06 +0500

    Chat_app

---
+++ app/src/main/java/com/example/pc/eme_chat/DataFish.java
@@ -0,0 +1,12 @@
+package com.example.pc.eme_chat;
+
+/**
+ * Created by pc on 5/21/2017.
+ */
+
+public class DataFish {
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+}
